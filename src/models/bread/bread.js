a53464dcdea358f9a784ed012774ae3ace915284
Allison Van Linden <allisonvanlinden@deltasource.eu>
2024-06-04 13:02:26 +0200

    add ordering methods

--- src/models/bread/bread.js
+++ src/models/bread/bread.js
@@ -6,7 +6,7 @@ import sandwichData from "../../common/fetch-sandwich-data.js";
  * Fetch all sandwich data and find the 'bread' entries
  * @param list - a collection of entries from a csv file containing sandwich data
  */
-function fetchBreadsFromList(list) {
+const fetchBreadsFromList = function(list) {
 	let breads = [];
 	for (const entry of list) {
 		if (entry.prijs >= 200) {
@@ -20,7 +20,7 @@ function fetchBreadsFromList(list) {
  * Find a bread's data by name
  * @param name {string} - the name of a bread
  */
-function fetchBreadByType(name = "geen") {
+const fetchBreadByName = function(name = "geen") {
 	const breads = fetchBreadsFromList(sandwichData);
 	if (breads.includes(name)) {
 		const sandwichWithPrice = sandwichData.find((entry) => entry.item === name);
@@ -28,4 +28,12 @@ function fetchBreadByType(name = "geen") {
 	}
 	return {bread: "geen", price: 0};
 }
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+exports.breads = fetchBreadsFromList;
+exports.fetchBreadByName = fetchBreadByName;
+
 
